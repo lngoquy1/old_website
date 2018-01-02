acdf5319193d5f7a7fafbc935debf784e7e90174
Lan <lngoquy1@swarthmore.edu>
2018-01-01 22:17:31 -0500

    Working on nav bar and modal still

--- site.js
+++ site.js
@@ -2,19 +2,30 @@
 // $('.navbutton').on('click', function() {
 // 	$('.content').toggleClass('isOpen');
 // });
-$(document).ready(function(){
-    $("#shelved").click(function(){
-        $("#shelvedModal").modal();
-    });
-});
 
-$('.cta a').click(function() {
-	$('html,body').animate({
-		scrollTop: $('#tours').offset().top
-	}, 500);
-	return false;
-});
+var closeButton = document.getElementByClassName("closebtn")[0];
+closeButton.onclick = function closeNav() {
+    document.getElementByClassName("sidebar").style.width = "0";
+    document.getElementByClassName("content").style.marginLeft= "0";
+}
 
-$('#tours li').on('click', function() {
-	$('#location').val($('img', this).attr('alt'));
-});
+var navButton = document.getElementByClassName("navbutton")[0];
+navButton.onclick = function openNav() {
+    document.getElementByClassName("sidebar").style.width = "250px";
+    document.getElementByClassName("content").style.marginLeft = "250px";
+}
+
+
+
+// document.getElementById("navbutton").onclick = openNav();
+
+// $('.cta a').click(function() {
+// 	$('html,body').animate({
+// 		scrollTop: $('#tours').offset().top
+// 	}, 500);
+// 	return false;
+// });
+
+// $('#tours li').on('click', function() {
+// 	$('#location').val($('img', this).attr('alt'));
+// });
